b37d572a59702d863785e02240ee3f1aa3ae3453
João Pedro <jp02120123@gmail.com>
2022-04-20 18:41:31 -0300

    feat: methods

---
+++ Classes/class04Methods/index.js
@@ -0,0 +1,19 @@
+let pessoas = {
+  nome: 'João Pedro',
+  idade: 23,
+  falar: function () {
+    console.log("hollo, my name is joão Pedro")    
+  },
+  aniversario() {
+    this.idade += 1;
+  },
+  dizerIdade() {
+    console.log('hii! my year old is ' + this.idade);
+  }
+}
+
+pessoas.aniversario();
+console.log(pessoas.idade);
+pessoas.aniversario();
+console.log(pessoas.idade);
+pessoas.dizerIdade();
